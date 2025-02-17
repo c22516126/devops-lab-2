092568327ec70fef1c0ea6b97b2adfc9ed7d7035
Jason Garcia <c22516126@mytudublin.ie>
2025-02-17 06:07:05 +0000

    average

--- hello.c
+++ hello.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
+    
+
     printf("Welcome!\n");
+
+    printf("Enter numbers, input a negative when finished\n");
+    int input = 0;
+    int total = 0;
+    int amount = 0;
+    int average = 0;
+
+    
+    while (input > -1)
+    {
+        scanf("%d", &input);
+        total += input;
+        amount++;
+    }
+
+    average = total / amount;
+    printf("Average: %d\n", average);
+
     return 0;
+    
 }
