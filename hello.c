9219dc618b7712ff213d69203f0c58228c4e2417
BeroCollege <Xdrr9728!>
2025-02-17 22:24:13 +0000

    Edited the average function file

--- hello.c
+++ hello.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
 
 int main() {
-    
-
     printf("Welcome!\n");
-
     printf("Enter numbers, input a negative when finished\n");
+    scanf("%d", &input);
+
     int input = 0;
     int total = 0;
     int amount = 0;
     int average = 0;
+    int largest, smallest;
 
-    
-    while (input > -1)
-    {
-        scanf("%d", &input);
-        total += input;
-        amount++;
+    if (input < 0) {
+        printf("No numbers entered.\n");
+        return 0;
     }
 
-    average = total / amount;
+    largest = smallest = input;
+    total += input;
+    amount++;
+
+    while (1) {
+        scanf("%d", &input);
+        
+        if (input < 0) // Stop if negative number is entered
+            break;
+        
+        total += input;
+        amount++;
+
+        if (input > largest)
+            largest = input;
+        if (input < smallest)
+            smallest = input;
+    }
+
+    if (amount > 0)
+        average = total / amount;
+    
     printf("Average: %d\n", average);
+    printf("Largest number: %d\n", largest);
+    printf("Smallest number: %d\n", smallest);
 
     return 0;
-    
 }
